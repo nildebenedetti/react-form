7dfb0024ed5fa17826394f337b00882b53c5fbb6
“Nilde <nilde.benedetti@gmail.com>
2026-05-04 15:23:18 +0200

    add: file articles.js con  array di objects contenti articoli in data folder

---
+++ src/data/articles.js
@@ -0,0 +1,22 @@
+const articles = [
+  {
+    title: 'Il mistero della violetta',
+    body: 'Sapevi che le violette contengono ionone? Questo composto "addormenta" temporaneamente i recettori dell’olfatto: dopo il primo profumo, l’odore sembra svanire nel nulla, per poi riapparire magicamente pochi minuti dopo.'
+  },
+  {
+    title: 'Il battito del Girasole',
+    body: 'I girasoli non seguono solo il sole. Questo fenomeno, chiamato eliotropismo, è guidato da un orologio circadiano interno. Di notte si riorientano verso est, pronti per l’alba, proprio come se avessero una sveglia biologica.'
+  },
+  {
+    title: 'L’inganno dell’Orchidea',
+    body: 'Alcune orchidee del genere Ophrys sono maestre del travestimento: i loro petali imitano perfettamente l’aspetto e l’odore delle femmine di certi insetti per attirare i maschi e garantire così l’impollinazione.'
+  },
+  {
+    title: 'La danza della Mimosa Pudica',
+    body: 'Nota come "sensitiva", questa pianta reagisce al tocco chiudendo istantaneamente le foglie su se stesse. È un meccanismo di difesa contro i predatori che serve a far apparire la pianta appassita e poco appetitosa.'
+  },
+  {
+    title: 'Il respiro della Regina della Notte',
+    body: 'Il Selenicereus grandiflorus è un cactus che fiorisce solo una volta all’anno e rigorosamente di notte. I suoi grandi fiori bianchi appassiscono prima del sorgere del sole, offrendo uno spettacolo rarissimo e profumatissimo.'
+  }
+];
