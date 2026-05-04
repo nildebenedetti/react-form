d7acacfa1f92de6af2480d5f78f648c6fdf7629b
“Nilde <nilde.benedetti@gmail.com>
2026-05-04 16:01:58 +0200

    add: il minimo sindacale di stile alla card i nCard.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,12 @@
-function Card( {
-    title="Il titolo del mio articolo",
-    body="Il corpo del mio articolo"
+function Card({
+    title = "Il titolo del mio articolo",
+    body = "Il corpo del mio articolo"
 }) {
-  return (
-    <div className="card">
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{body}</p>
-  </div>
-</div>
-  )
+    return <div className="card p-2 m-4">
+        <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{body}</p>
+        </div>
+    </div>
 }
 export default Card
