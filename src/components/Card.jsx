ba9666fedd98259a4fda7600d5e16521497229ee
“Nilde <nilde.benedetti@gmail.com>
2026-05-04 22:03:57 +0200

    add button per eliminare articoli in card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,13 @@ function Card({
     body = "Il corpo del mio articolo"
 }) {
     return <div className="card p-2 m-4">
+        <div className="d-flex justify-content-end mt-3">
+          <button type="button" className="btn btn-outline-danger">
+            {/* Esempio con l'icona del cestino (trash) */}
+            <i className="bi bi-trash3 me-2"></i>
+            Elimina
+          </button>
+        </div>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{body}</p>
