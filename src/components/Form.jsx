b4ac26933e8694322d1446cf886d2ae78e963415
“Nilde <nilde.benedetti@gmail.com>
2026-05-04 16:25:47 +0200

    fix: className al posto di class e htmlFor invece di For + correzione campo type di form title in Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,16 +2,16 @@ function Form() {
     return <div className="card p-2 m-4">
         <form>
             <div className="mb-3">
-                <label for="articleTitle" className="form-label p-2">Aggiungi un nuovo articolo</label>
-                <input type="articleTitle" className="form-control" id="articleTitle" aria-describedby="articleTitle" />
+                <label htmlFor="articleTitle" className="form-label p-2">Aggiungi un nuovo articolo</label>
+                <input type="text" className="form-control" id="articleTitle" aria-describedby="articleTitle" />
                 <div id="articleTitle" className="form-text">Inserisci il titolo del tuo articolo.</div>
             </div>
-            <div class="form-floating">
+            <div className="form-floating">
                 <textarea className="form-control" placeholder="Il tuo articolo" id="floatingTextarea"></textarea>
-                <label for="floatingTextarea">Scrivi il tuo articolo...</label>
+                <label htmlFor="floatingTextarea">Scrivi il tuo articolo...</label>
             </div>
             <div className="my-3">
-                <button type="submit" class="btn btn-primary">Aggiungi</button>
+                <button type="submit" className="btn btn-primary">Aggiungi</button>
             </div>
         </form>
     </div>
