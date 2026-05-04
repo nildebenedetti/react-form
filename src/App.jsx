6e05bb0f311e0241fb3531fd8a920edc9ec3d633
“Nilde <nilde.benedetti@gmail.com>
2026-05-04 17:03:46 +0200

    remove: import di form component per semplificare tutto in app + add: variabile di stato con stringa vuota in App function scope per settarevalue dell;input destinato al titolo - App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Card from "./components/Card";
 import articles from "./data/articles";
 import Form from "./components/Form";
 
 function App() {
-  const articleCardList = articles.map( article => {
+    // inserisco stringa vuota per il titolo
+    const [ titolo, setTitolo] = useState('');
+  
+
+  const articleCardList = articles.map(article => {
     return <li key={crypto.randomUUID()}>
       <Card
-      title={article.title}
-      body={article.body}
+        title={article.title}
+        body={article.body}
       />
     </li>
   })
@@ -15,7 +20,22 @@ function App() {
     <ul className="list-unstyled">
       {articleCardList}
     </ul>
-    <Form/>
+    <div className="card p-2 m-4">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="articleTitle" className="form-label p-2">Aggiungi un nuovo articolo</label>
+          <input type="text"  value={titolo} className="form-control" id="articleTitle" aria-describedby="articleTitle" />
+          <div id="articleTitle" className="form-text">Inserisci il titolo del tuo articolo.</div>
+        </div>
+        <div className="form-floating">
+          <textarea className="form-control" placeholder="Il tuo articolo" id="floatingTextarea"></textarea>
+          <label htmlFor="floatingTextarea">Scrivi il tuo articolo...</label>
+        </div>
+        <div className="my-3">
+          <button type="submit" className="btn btn-primary">Aggiungi</button>
+        </div>
+      </form>
+    </div>
   </div>
 }
 export default App;
